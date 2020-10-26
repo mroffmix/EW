f0a84817758a00a2fa4d96e1a77e58aed2a7eb9f
Ilya Mikhailov <mihailoov@gmail.com>
2020-10-26 14:30:17 +0100

    Initial Commit

---
+++ EW/EWApp.swift
@@ -0,0 +1,17 @@
+//
+//  EWApp.swift
+//  EW
+//
+//  Created by Ilya on 26.10.20.
+//
+
+import SwiftUI
+
+@main
+struct EWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
